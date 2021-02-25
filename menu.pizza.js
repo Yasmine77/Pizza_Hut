39972fdfdab7e78e90e0e58fe21e29e8d9ecf4ab
unknown <Yasmine77>
2021-02-25 14:54:13 +0100

    mon premier commit

---
+++ menu.pizza.js
@@ -0,0 +1,68 @@
+/* JQUERY */
+$('#img1').mouseover(function () {
+    $('#description1').show();
+});
+$('#img1').mouseout(function () {
+    $('#description1').hide();
+});
+
+/* JQUERY */
+$('#img2').mouseover(function () {
+    $('#description2').show();
+});
+$('#img2').mouseout(function () {
+    $('#description2').hide();
+});
+
+/* JQUERY */
+$('#img3').mouseover(function () {
+    $('#description3').show();
+});
+$('#img3').mouseout(function () {
+    $('#description3').hide();
+});
+
+/* JQUERY */
+$('#img4').mouseover(function () {
+    $('#description4').show();
+});
+$('#img4').mouseout(function () {
+    $('#description4').hide();
+});
+$('#img5').mouseover(function () {
+    $('#description5').show();
+});
+$('#img5').mouseout(function () {
+    $('#description5').hide();
+});
+$('#img6').mouseover(function () {
+    $('#description6').show();
+});
+$('#img6').mouseout(function () {
+    $('#description6').hide();
+});
+$('#img7').mouseover(function () {
+    $('#description7').show();
+});
+$('#img7').mouseout(function () {
+    $('#description7').hide();
+});
+$('#img8').mouseover(function () {
+    $('#description8').show();
+});
+$('#img8').mouseout(function () {
+    $('#description8').hide();
+});
+$('#img9').mouseover(function () {
+    $('#description9').show();
+});
+$('#img9').mouseout(function () {
+    $('#description9').hide();
+});
+$('#img10').mouseover(function () {
+    $('#description10').show();
+});
+$('#img10').mouseout(function () {
+    $('#description10').hide();
+});
+
